b77896b504bf26c08478d4e961ffed37a7d7ec89
Demi Ogun <demiogun1@gmail.com>
2021-06-28 15:16:59 +0100

    Server config

---
+++ server/src/index.ts
@@ -0,0 +1,71 @@
+import 'reflect-metadata'
+import { MikroORM } from '@mikro-orm/core'
+import { ApolloServer } from 'apollo-server-express'
+import express from 'express'
+import { buildSchema } from 'type-graphql'
+import cors from 'cors'
+
+import { __prod__ } from './constants'
+import mikroOrmConfig from './mikro-orm.config'
+import { PostResolver } from './resolvers/post'
+import { UserResolver } from './resolvers/user'
+
+import redis from 'redis'
+import session from 'express-session'
+import connectReddis from 'connect-redis'
+import { OrmContext } from './types'
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig)
+  await orm.getMigrator().up()
+
+  const app = express()
+
+  const RedisStore = connectReddis(session)
+  const redisClient = redis.createClient()
+
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    })
+  )
+
+  app.use(
+    session({
+      name: 'qid',
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365, //1 year in milliseconds
+        httpOnly: true,
+        sameSite: 'lax',
+        secure: __prod__, // cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: 'temporarysecretkey',
+      resave: false,
+    })
+  )
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: ({ req, res }): OrmContext => ({ em: orm.em, req, res }),
+  })
+
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  })
+
+  app.listen(4000, () => {
+    console.log('Server started on localhost:4000')
+  })
+}
+
+main().catch((err) => console.error(err))
