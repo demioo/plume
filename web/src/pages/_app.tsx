4fb0eee98b6c0cc5398a150c9b428c2adf8a1b7a
Demi Ogun <demiogun1@gmail.com>
2021-07-05 11:50:10 +0100

    Refactor app component

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,95 +1,17 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
-import { Provider, createClient, dedupExchange, fetchExchange } from 'urql'
-import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache'
-
 import theme from '../theme'
-import {
-  LoginMutation,
-  LogoutMutation,
-  MeDocument,
-  MeQuery,
-  RegisterMutation,
-} from '../generated/graphql'
-
-function updateQuery<Result, Query>(
-  cache: Cache,
-  queryInput: QueryInput,
-  result: any,
-  fn: (r: Result, q: Query) => Query
-) {
-  return cache.updateQuery(queryInput, (data) => fn(result, data as any) as any)
-}
-
-const client = createClient({
-  url: 'http://localhost:4000/graphql',
-  fetchOptions: {
-    credentials: 'include',
-  },
-  exchanges: [
-    dedupExchange,
-    cacheExchange({
-      updates: {
-        Mutation: {
-          login: (_result, _args, cache, _info) => {
-            updateQuery<LoginMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                if (result.login.errors) {
-                  return query
-                } else {
-                  return {
-                    me: result.login.user,
-                  }
-                }
-              }
-            )
-          },
-          register: (_result, _args, cache, _info) => {
-            updateQuery<RegisterMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                if (result.register.errors) {
-                  return query
-                } else {
-                  return {
-                    me: result.register.user,
-                  }
-                }
-              }
-            )
-          },
-          logout: (_result, _args, cache, _info) => {
-            updateQuery<LogoutMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              () => ({ me: null })
-            )
-          },
-        },
-      },
-    }),
-    fetchExchange,
-  ],
-})
 
 function MyApp({ Component, pageProps }: any) {
   return (
-    <Provider value={client}>
-      <ChakraProvider resetCSS theme={theme}>
-        <ColorModeProvider
-          options={{
-            useSystemColorMode: true,
-          }}
-        >
-          <Component {...pageProps} />
-        </ColorModeProvider>
-      </ChakraProvider>
-    </Provider>
+    <ChakraProvider resetCSS theme={theme}>
+      <ColorModeProvider
+        options={{
+          useSystemColorMode: true,
+        }}
+      >
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ChakraProvider>
   )
 }
 
